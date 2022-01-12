dfa3914459df433243aeb16f577ef0f153626515
milyasarmans <milyasarmans13@gmail.com>
2022-01-13 03:43:27 +0700

    up

--- application/models/M_hasil.php
+++ application/models/M_hasil.php
@@ -28,18 +28,6 @@ class M_hasil extends CI_Model
         return $query->result();
     }
 
-    public function cetak($id)
-    {
-        $this->db->select('*');
-        $this->db->from('tb_peserta');
-        $this->db->join('tb_matapelajaran', 'tb_peserta.id_matapelajaran=tb_matapelajaran.id_matapelajaran');
-        $this->db->join('tb_jenis_ujian', 'tb_peserta.id_jenis_ujian=tb_jenis_ujian.id_jenis_ujian');
-        $this->db->join('tb_siswa', 'tb_peserta.id_siswa=tb_siswa.id_siswa');
-        $this->db->where('tb_peserta.id_peserta', $id);
-        $query = $this->db->get();
-        return $query->result();
-    }
-
    
 }
 
